315b51f10640be8ca3a2bad73c0b269a57148485
jay solanki <jaysolanki46@gmail.com>
2019-10-31 02:01:48 +1300

    Initial commit/Bootstrap Added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pita_Pit_Inventory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
